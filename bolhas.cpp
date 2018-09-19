6139b0e1a25d1f943f8ca61887e841c3c63178c5
Dina Livia <dinalivia@bct.ect.ufrn.br>
2018-09-18 21:46:34 -0300

    Exercise 3 - Labeling

--- bolhas.cpp
+++ bolhas.cpp
@@ -3,39 +3,82 @@
 
 using namespace cv;
 
-int main(int argc, char** argv){
+int main(){
   Mat image, mask;
   int width, height;
-  int nobjects;
-  
+  int nobjects, holes, counter;
+
+  // - Reading image --  
   CvPoint p;
   image = imread("img/bolhas.png",CV_LOAD_IMAGE_GRAYSCALE);
   
   if(!image.data){
-    std::cout << "imagem nao carregou corretamente\n";
+    std::cout << "Couldn't load the file correctly\n";
     return(-1);
   }
+
   width=image.size().width;
   height=image.size().height;
 
-  p.x=0;
-  p.y=0;
+  p.x = 0;
+  p.y = 0;
 
-  // busca objetos com buracos presentes
-  nobjects=0;
+  //  -- Excluding objects that touch the first and last lines --
+  for(int i=0; i<height; i++){
+    if(image.at<uchar>(0, i) == 255){ 
+        floodFill(image,Point(i, 0), 0); 
+    }
+    if(image.at<uchar>(width-1, i) == 255){ 
+        floodFill(image,Point(i, width-1), 0);
+    }
+  } 
+
+  //  -- Excluding objects that touch the first and last columns --
+  for(int i=0; i<width; i++){
+    if(image.at<uchar>(i,0) == 255){ 
+        floodFill(image,Point(0, i), 0);
+    }
+    if(image.at<uchar>(i,height-1) == 255){ 
+        floodFill(image,Point(height-1, i), 0);
+    }
+  }
+
+  //  -- Looking for objects and labeling them --
+  nobjects=0; 
   for(int i=0; i<height; i++){
     for(int j=0; j<width; j++){
-      if(image.at<uchar>(i,j) == 255){
-		// achou um objeto
-		nobjects++;
-		p.x=j;
-		p.y=i;
-		floodFill(image,p,nobjects);
-	  }
-	}
+      if(image.at<uchar>(i,j) == 255){ 
+        // Found an white object 
+        nobjects++;
+        p.x=j;
+        p.y=i;
+        floodFill(image,p,nobjects);
+      }
+    }
   }
-  imshow("image", image);
+
+ //  -- Changing background Color --
+  floodFill(image,Point(0,0),255);
+
+ //  -- Looking for bubbles with holes --
+  holes = 0; counter = 1;
+  for(int i=0; i<height; i++){
+    for(int j=0; j<width; j++){
+      if(image.at<uchar>(i,j) == 0 && (int)image.at<uchar>(i,j-1)>counter){ //se encontrar um buraco e já não tiver contado a bolha
+  		// Found an bubble with a hole 
+  		counter++;
+  		p.x=j-1;
+  		p.y=i;
+  		floodFill(image,p,counter);
+  	  }
+  	}
+  }
+ // floodFill(image,p,0); // se quiser retornar o fundo pra preto
+
+  std:: cout << "Number of bubbles: " <<  nobjects << " and number of bubbles with holes: " << counter <<std::endl;
+
   imwrite("labeling.png", image);
+  imshow("janela", image); 
   waitKey();
   return 0;
-}
+} 
